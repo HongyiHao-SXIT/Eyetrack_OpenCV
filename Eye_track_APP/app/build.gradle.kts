a93c6172cf62a394103b566b007ae25b35dc0f75
HongyiHao-SXIT <Lanyi_adict@outlook.com>
2025-04-19 17:32:56 +0800

    add Ubuntu script

--- Eye_track_APP/app/build.gradle.kts
+++ Eye_track_APP/app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
     implementation 'org.opencv:opencv-android:4.5.5'
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
